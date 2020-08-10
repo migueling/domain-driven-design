32a45b5f50b634e1f123b26747db371dda0039fb
Fernando Dingler <ferdingler@gmail.com>
2020-08-09 22:02:37 -0700

    Category value object

---
+++ src/main/java/com/ferdingler/domain/model/Category.java
@@ -0,0 +1,33 @@
+package com.ferdingler.domain.model;
+
+import java.util.Objects;
+
+public class Category {
+
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
