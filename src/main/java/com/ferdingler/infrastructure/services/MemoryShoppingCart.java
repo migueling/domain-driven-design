e2a09eed05617ff5aa8a40e361670a5b9214a912
Fernando Dingler <ferdingler@gmail.com>
2020-08-15 18:26:18 -0700

    Finished part 1

--- src/main/java/com/ferdingler/infrastructure/services/MemoryShoppingCart.java
+++ src/main/java/com/ferdingler/infrastructure/services/MemoryShoppingCart.java
@@ -9,32 +9,45 @@ import java.util.List;
 
 public class MemoryShoppingCart implements ShoppingCart {
 
+    private final int MAX_PRODUCTS = 10;
+    private final List<Product> products;
+
+    public MemoryShoppingCart() {
+        this.products = new ArrayList<>();
+    }
+
     /**
      * Note: A maximum of 10 products should be allowed in the cart.
      * Throw an exception if more than 10 products are added.
      */
     @Override
     public void addProduct(Product product) {
+        if(products.size() >= MAX_PRODUCTS) {
+            throw new RuntimeException("Sorry, no more than 10 products");
+        }
 
+        products.add(product);
     }
 
     @Override
     public void removeProduct(Sku sku) {
-
+        products.removeIf(product -> product.getSku().equals(sku));
     }
 
     @Override
     public List<Product> getProducts() {
-        return new ArrayList<>();
+        return products;
     }
 
     @Override
     public int getCartSize() {
-        return 0;
+        return products.size();
     }
 
     @Override
     public double calculateCheckoutTotal() {
-        return 0;
+        return products.stream()
+                .map(product -> product.getPrice().getValue())
+                .reduce(0.0, Double::sum);
     }
 }
--- src/test/java/com/ferdingler/infrastructure/services/MemoryShoppingCartTest.java
+++ src/test/java/com/ferdingler/infrastructure/services/MemoryShoppingCartTest.java
@@ -1,33 +1,130 @@
 package com.ferdingler.infrastructure.services;
 
+import com.ferdingler.domain.model.Price;
 import com.ferdingler.domain.model.Product;
 import com.ferdingler.domain.services.ShoppingCart;
 import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 class MemoryShoppingCartTest {
 
-    @Test
-    public void newCartStartsWithZeroProducts() {
-        ShoppingCart cart = new MemoryShoppingCart();
-        assertEquals(0, cart.getProducts().size());
+    private Product product;
+    private ShoppingCart cart;
+
+    @BeforeEach
+    void setUp() {
+        this.cart = new MemoryShoppingCart();
+    }
+
+    Product randomProduct() {
+        return Product.buildNewProduct(
+                "Dog Food",
+                "Pet Supplies",
+                5
+        );
     }
 
     @Test
-    public void newCartHasZeroCheckoutTotal() {
-        ShoppingCart cart = new MemoryShoppingCart();
+    void newCartStartsWithZeroProducts() {
+        assertEquals(0, cart.getCartSize());
+    }
+
+    @Test
+    void newCartHasZeroCheckoutTotal() {
         assertEquals(0, cart.calculateCheckoutTotal());
     }
 
     @Test
-    public void addOneProductToCart() {
-        Product product = Product.buildNewProduct("Dog Food", "Pet Supplies", 5);
-        ShoppingCart cart = new MemoryShoppingCart();
+    void addOneProductToCart() {
+        Product product = randomProduct();
         cart.addProduct(product);
-        assertEquals(1, cart.getProducts().size());
+        assertEquals(1, cart.getCartSize());
     }
 
+    @Test
+    void addedProductCanBeRetrieved() {
+        Product product = randomProduct();
+        cart.addProduct(product);
+
+        List<Product> productList = cart.getProducts();
+        assertEquals(product, productList.get(0));
+    }
+
+    @Test
+    void noMoreThan10ProductsShouldBeAllowed() {
+        for(int i=0; i<10; i++) {
+            cart.addProduct(randomProduct());
+        }
+
+        assertThrows(RuntimeException.class, () -> {
+            cart.addProduct(randomProduct());
+        });
+    }
+
+    @Test
+    void removingProductDecreasesCartSize() {
+        Product product = randomProduct();
+        cart.addProduct(product);
+        assertEquals(1, cart.getCartSize());
+
+        cart.removeProduct(product.getSku());
+        assertEquals(0, cart.getCartSize());
+    }
+
+    @Test
+    void productIsRemovedFromCart() {
+        Product product = randomProduct();
+        cart.addProduct(product);
+        cart.removeProduct(product.getSku());
+
+        List<Product> productList = cart.getProducts();
+        assertFalse(productList.contains(product));
+    }
+
+    @Test
+    void theCorrectProductIsRemovedFromCart() {
+        Product productOne = randomProduct();
+        Product productTwo = randomProduct();
+
+        cart.addProduct(productOne);
+        cart.addProduct(productTwo);
+        cart.removeProduct(productOne.getSku());
+
+        List<Product> productList = cart.getProducts();
+        assertFalse(productList.contains(productOne));
+        assertTrue(productList.contains(productTwo));
+    }
+
+    @Test
+    void checkoutTotalIsCalculatedForOneProduct() {
+        Product product = randomProduct();
+        Price price = product.getPrice();
+
+        cart.addProduct(product);
+
+        assertEquals(price.getValue(), cart.calculateCheckoutTotal());
+    }
+
+    @Test
+    void checkoutTotalIsCalculatedForMultipleProducts() {
+        int numProducts = 1 + new Random().nextInt(10);
+        for(int i=0; i<numProducts; i++) {
+            cart.addProduct(randomProduct());
+        }
+
+        List<Product> productList = cart.getProducts();
+        double expectedTotal = 0;
+        for(Product product : productList) {
+            expectedTotal += product.getPrice().getValue();
+        }
+
+        assertEquals(expectedTotal, cart.calculateCheckoutTotal());
+    }
 }
